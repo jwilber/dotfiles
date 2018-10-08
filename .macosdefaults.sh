bdb910870d1fca80e0d4f0f8968a9903a3a2c310
Jared Wilber <j.wilber92@gmail.com>
2018-10-07 22:43:12 -0700

    removed screenshot type

--- .macosdefaults.sh
+++ .macosdefaults.sh
@@ -7,8 +7,5 @@ defaults write com.apple.dock autohide -bool true
 # Save screenshots to the desktop
 defaults write com.apple.screencapture location -string “$HOME/Desktop”
 
-# Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
-defaults write com.apple.screencapture type -string “png”
-
 # Display full POSIX path as Finder window title
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
